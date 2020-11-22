fbab171692d5447d3eed51c8a0ab6f90dfd22bf2
YANISA PONGMETHA <63010249@KMITL.AC.TH>
2020-11-22 21:34:17 +0700

    Add project files.

---
+++ Factor/Source.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int strNum(char* str) {
+    int i, x = 0;
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9')x++;
+    }
+    if (x == strlen(str))return 1;
+    else return 0;
+}
+int main() {
+    char num[100];
+    int a = 0, k = 2, i;
+    int* prt = &a;
+    printf("Enter number : ");
+    scanf("%s", num);
+    if (strNum(num) == 1) {
+        for (i = 0; i < strlen(num); i++) {
+            (*prt) = (num[i] - '0') + (*prt) * 10;
+        }
+        if ((*prt) < 2)printf("The number of input is less than 1");
+        else {
+            printf("Factor is : ");
+            while ((*prt) != 1) {
+                while ((*prt) % k == 0) {
+                    printf("%d", k);
+                    (*prt) /= k;
+                    if ((*prt) == 1)break;
+                    printf(" x ");
+                }
+                k++;
+            }
+        }
+    }
+    else printf("ERROR !!");
+    return 0;
+}
